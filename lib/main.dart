e72bea404980c8124a5f16a13c634f2a3ecb0fd3
tifani3112 <102417496+tifani3112@users.noreply.github.com>
2023-09-15 21:58:34 +0700

    finished project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:timer/timer.dart';
+
+void main() => runApp(StopwatchApp());
+
+class StopwatchApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
